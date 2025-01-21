3fe33b27884bd7cc110d54d176ee10dd5448a048
SvenYorha <diamondraiderone@gmail.com>
2025-01-21 18:23:54 +0000

    base and temp item

---
+++ src/main/java/net/svenyorhavich/modulartech/item/Moditems.java
@@ -0,0 +1,18 @@
+package net.svenyorhavich.modulartech.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.svenyorhavich.modulartech.ModularTech;
+
+public class Moditems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ModularTech.MOD_ID);
+
+    public static final DeferredItem<Item> PLATE_IRON = ITEMS.register("iron_plate",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
